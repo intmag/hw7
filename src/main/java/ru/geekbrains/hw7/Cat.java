318f2718740617be05020700c39a965c3295f666
Evgeniy <e.kurynkin@gmail.com>
2021-03-23 00:22:11 +0300

    done

--- src/main/java/ru/geekbrains/hw7/Cat.java
+++ src/main/java/ru/geekbrains/hw7/Cat.java
@@ -1,4 +1,25 @@
 package ru.geekbrains.hw7;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate p) {
+        if (p.getFood() >= appetite && !satiety) {
+            p.decreaseFood(appetite);
+            satiety = true;
+            System.out.println("Котик (^˵◕ω◕˵^) " + name + " съел из миски " + appetite);
+        } else if (satiety) {
+            System.out.println("Котик (^˵◕ω◕˵^) " + name + " сытый");
+        } else if (p.getFood() < appetite) {
+            System.out.println("Котик (^˵◕ω◕˵^) " + name + " не желает есть так мало.");
+        }
+    }
+
 }
